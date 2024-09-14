7700e1f18a43b5606a56a8b62c5695f239afaa4b
joaofan11 <joaofan11@gmail.com>
2024-09-13 22:51:14 -0400

    Exercicios

---
+++ ex016.py
@@ -0,0 +1,2 @@
+#Crie um programa que leia um número real qualquer pleo tecaldo
+#e mostre na tela su porção inteira. Ex: Digite um númeto: 6.127. O número 6.127 tem a parte inteira 6.
---
+++ ex018.py
@@ -0,0 +1,2 @@
+# Faça um programa que leia um ângulo qualquer e mostre na tela o
+# seno, cosseno e a tangente desse angulo
